e50fa3bf455712148041fcc9f6a73b1809351dc5
Rufaida Khan <rk3963@nyu.edu>
2022-05-31 14:32:39 -0400

    Update new_styles.js

--- Ws/JS/new_styles.js
+++ Ws/JS/new_styles.js
@@ -1,10 +1,11 @@
 let date = new Date(); //Get current Date
 let day = date.getDay();
 let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
 console.log(date + ", " + week[day]);
 let month = date.getMonth() + 1;
 let weekDay = document.querySelector('h3');
-weekDay.textContent = date.getDate() + "/" + month + "/" + date.getFullYear() + ", " + week[day];
+weekDay.textContent = week[day] + ", " + date.getDate() + "th " + months[month] + "" + date.getFullYear() + ", ";
 
 
 function dark() {
