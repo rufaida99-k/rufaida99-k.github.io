f1bf2440f4d0766fbbd9cdcb24aeb7951cee88bf
Rufaida Khan <83421716+ruf99@users.noreply.github.com>
2022-08-24 15:33:56 -0400

    Update new_styles.js

--- R_R/JS/new_styles.js
+++ R_R/JS/new_styles.js
@@ -16,3 +16,13 @@ $("#switch").on("click", () => {
     $(".body").removeClass("dark");
   }
 })
+
+const darkMode = document.querySelector(".theme-toggle");
+
+function darkify() {
+  document.documentElement.classList.toggle("theme--night");
+}
+
+darkMode.addEventListener("click", darkify);
+
+darkify();
