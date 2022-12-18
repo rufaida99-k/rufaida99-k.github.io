2dcf440941abfa81a2e33e4ade49aef5e5caba42
Rufaida Khan <83421716+ruf99@users.noreply.github.com>
2022-12-17 22:29:02 -0500

    Update new_styles.js

--- R_R/JS/new_styles.js
+++ R_R/JS/new_styles.js
@@ -3,10 +3,9 @@ let day = date.getDay();
 let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let months = ["December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
 console.log(date + ", " + week[day]);
-let month = date.getMonth() + 1;
-console.log("month: " + month);
+let month = date.getMonth();
 let weekDay = document.querySelector('h3');
-weekDay.textContent = week[day] + ", " + date.getDate() + " " + months[month] + " " + date.getFullYear();
+weekDay.textContent = week[day] + ", " + date.getDate() + " " + month + " " + date.getFullYear();
 
 
 
